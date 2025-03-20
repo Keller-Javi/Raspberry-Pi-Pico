c5944c35ff9ac71b5b330a649537e3cc596d6643
Keller-Javi <kellerjavier13@gmail.com>
2025-03-20 15:20:57 -0300

    Primer probada de pico

--- main.py
+++ main.py
@@ -6,10 +6,10 @@ sleep(1)    #le damos tiempo a vREPL
 print("\nLED esta destellando...")
 while True:
     try:
-        led_board.toggle()
-        # led_board.value(not led_board.value())
-        sleep(.5) # sleep 1sec
-    except KeyboardInterrupt:
+        led_board.toggle() # opsión unica para el pico
+        # led_board.value(not led_board.value()) # Lo mismo pero también anda en el esp32, escribe el estado negado
+        sleep(.25)
+    except KeyboardInterrupt: # Except es para que no salte un mensaje raro al terminar el programa con ctrl + c
         break
 led_board.off()
 print("Listo")
