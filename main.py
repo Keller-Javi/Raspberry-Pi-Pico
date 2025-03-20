282befdf100cff932638b1f2e713131f44f0ae63
Keller-Javi <kellerjavier13@gmail.com>
2025-03-20 16:36:22 -0300

    Asynk prueba

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import asyncio
 from machine import Pin
 from led_async import LED_async  # Class as listed above
 
-async def main():
+async def main(): # Se define como una coroutine
     pines = [14, 17]
     leds = [LED_async(n) for n in pines]
     for n, led in enumerate(leds):
@@ -18,6 +18,14 @@ async def main():
         led.off()
 
 try:
-    asyncio.run(main())
+    asyncio.run(main()) # Ejecutamos la coroutine 
 except KeyboardInterrupt:
     print('Keyboard interrupt at loop level.')
+
+# await async.sleep(delay) duerme la función por ese tiempo y otra función puede ejecutarse en ese tiempo 
+
+# await hace que se duerma por un tiempo y si hay otra función esperando, esta se ejecuta
+#   hasta que se despierte la otra función
+
+# para que sean concurrentes hay que utilizar tareas 
+#   (Las tareas se ven dentro del archivo led_async.py)
